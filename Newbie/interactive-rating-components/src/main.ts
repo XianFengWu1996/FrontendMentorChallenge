73c4e68cfade84e0a218359a5a5a19d0daafdc2e
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-03-15 12:46:53 -0400

    finish active states for buttons

--- Newbie/interactive-rating-components/src/main.ts
+++ Newbie/interactive-rating-components/src/main.ts
@@ -1,3 +1,26 @@
-console.log('hello world');
+let rating: string = '';
 
-export {};
+const btns = document.querySelectorAll('.card--rating-item');
+
+function onClickHandler(this: HTMLButtonElement) {
+  // step 1
+  // remove the card--btn-selected class from all buttons
+  btns.forEach((btn) => {
+    btn.classList.remove('card--btn-selected');
+  });
+
+  // step 2
+  // add the card--btn-selected class to the button thats clicked
+  if (this.value === rating) {
+    this.classList.remove('card--btn-selected');
+    rating = '';
+  } else {
+    this.classList.add('card--btn-selected');
+    rating = this.value;
+  }
+}
+
+// attach event listener for all individual buttons
+btns.forEach((btn) => {
+  btn.addEventListener('click', onClickHandler);
+});
